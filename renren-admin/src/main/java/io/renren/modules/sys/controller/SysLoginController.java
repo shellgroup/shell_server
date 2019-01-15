f5dc58b0ccba5a47d9718a2e3c250b3a3fc8bcb8
shuaikangzhang <303316861@qq.com>
2019-01-15 14:12:11 +0800

    修改内容： 1.更新验证码生成机制

--- renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -17,23 +17,20 @@
 package io.renren.modules.sys.controller;
 
 
+
 import com.google.code.kaptcha.Constants;
-import com.google.code.kaptcha.Producer;
 import io.renren.common.utils.R;
+import io.renren.common.utils.VerifyCodeUtils;
 import io.renren.modules.sys.shiro.ShiroUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Map;
 
@@ -46,23 +43,27 @@ import java.util.Map;
  */
 @Controller
 public class SysLoginController {
-	@Autowired
-	private Producer producer;
-
 	@RequestMapping("captcha.jpg")
 	public void captcha(HttpServletResponse response)throws IOException {
         response.setHeader("Cache-Control", "no-store, no-cache");
         response.setContentType("image/jpeg");
-		//生成文字验证码
-		String text = producer.createText();
-		System.out.println(text+"   _____________");
-        //生成图片验证码
-        BufferedImage image = producer.createImage(text);
-        //保存到shiro session
-        ShiroUtils.setSessionAttribute(Constants.KAPTCHA_SESSION_KEY, text);
+		try {
+			response.setHeader("Pragma", "No-cache");
+			response.setHeader("Cache-Control", "no-cache");
+			response.setDateHeader("Expires", 0);
+			response.setContentType("image/jpg");
 
-        ServletOutputStream out = response.getOutputStream();
-        ImageIO.write(image, "jpg", out);
+			//生成随机字串
+			String verifyCode = VerifyCodeUtils.generateVerifyCode(5);
+			//存入会话session
+			ShiroUtils.setSessionAttribute(Constants.KAPTCHA_SESSION_KEY, verifyCode);
+			//生成图片
+			int w = 146, h = 33;
+			VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	/**
