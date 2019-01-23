35cf87b95eb118bb5c5801da687a34cde009dd1b
shuaikangzhang <303316861@qq.com>
2019-01-23 14:21:56 +0800

    修改内容： 1.增加本地图标的查询方法

---
+++ renren-admin/src/main/java/io/renren/modules/sys/dao/SysIconDao.java
@@ -0,0 +1,11 @@
+package io.renren.modules.sys.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.renren.modules.sys.entity.SysIconEntity;
+
+/**
+ * @author:zsk
+ * @CreateTime:2019-01-23 11:43
+ */
+public interface SysIconDao extends BaseMapper<SysIconEntity> {
+}
---
+++ renren-admin/src/main/java/io/renren/modules/sys/service/impl/SysIconServiceImpl.java
@@ -0,0 +1,16 @@
+package io.renren.modules.sys.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.renren.modules.sys.dao.SysIconDao;
+import io.renren.modules.sys.entity.SysIconEntity;
+import io.renren.modules.sys.service.SysIconService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author:zsk
+ * @CreateTime:2019-01-23 11:45
+ */
+@Service("sysIconService")
+public class SysIconServiceImpl extends ServiceImpl<SysIconDao, SysIconEntity> implements SysIconService {
+
+}
