446e53828fab70d5b28c8e37eb245afc6ebf2792
shuaikangzhang <303316861@qq.com>
2019-03-07 15:07:11 +0800

    修改内容： 1.修改导入包和方法（原来的包和方法随着版本升级已经被移除）

--- master-admin/src/main/java/com/winnerdt/common/utils/base64ToMultipartFile/Base64MultipartFileUtil.java
+++ master-admin/src/main/java/com/winnerdt/common/utils/base64ToMultipartFile/Base64MultipartFileUtil.java
@@ -1,9 +1,9 @@
 package com.winnerdt.common.utils.base64ToMultipartFile;
 
 import org.springframework.web.multipart.MultipartFile;
-import sun.misc.BASE64Decoder;
 
-import java.io.IOException;
+import java.util.Base64;
+import java.util.Base64.Decoder;
 
 /**
  * @author:zsk
@@ -15,9 +15,9 @@ public class Base64MultipartFileUtil {
         try {
             String[] baseStrs = base64.split(",");
 
-            BASE64Decoder decoder = new BASE64Decoder();
+            Decoder decoder = Base64.getDecoder();
             byte[] b = new byte[0];
-            b = decoder.decodeBuffer(baseStrs[1]);
+            b = decoder.decode(baseStrs[1]);
 
             for(int i = 0; i < b.length; ++i) {
                 if (b[i] < 0) {
@@ -26,7 +26,7 @@ public class Base64MultipartFileUtil {
             }
 
             return new BASE64DecodedMultipartFile(b, baseStrs[0]);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
